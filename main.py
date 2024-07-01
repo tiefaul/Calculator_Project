c812f4b9af98300ec7ed73a31a999d24622bd6c1
TieFaul <tylerfaulhaber@gmail.com>
2024-06-30 21:01:01 -0500

    Added subtraction

--- main.py
+++ main.py
@@ -41,23 +41,31 @@ button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_cl
 button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = tk.Button(root, text="+", padx=39, pady=20, command=lambda: button_click("+"))
+button_subtract = tk.Button(root, text="-", padx=39, pady=20, command=lambda: button_click("-"))
 button_delete = tk.Button(root, text="C", padx=39, pady=20, command=button_clear)
+button_total = tk.Button(root, text="=", padx=39, pady=20, command=button_equal)
 
 # Place buttons on the grid
+
+# Bottom Row
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
+button_add.grid(row=3, column=3)
 
+# Middle Row
 button_4.grid(row=2, column=0)
 button_5.grid(row=2, column=1)
 button_6.grid(row=2, column=2)
+button_subtract.grid(row=2, column=3)
 
+# Top Row
 button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=1)
-button_add.grid(row=4, column=2)
 button_delete.grid(row=4, column=0)
+button_total.grid(row=4, column=3)
 
 root.mainloop()
