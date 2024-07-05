b3f3c4ff1f6054a0f043e714f71e7adf1c10136e
TieFaul <tylerfaulhaber@gmail.com>
2024-07-05 18:44:15 -0500

    cleaned up mess

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ root.title("Tyler's Calculator")
 root.resizable(False, False)
 
 # Create display widget
-display = ttk.Frame(root)
-editFrame = ttk.Frame(display, borderwidth=5, relief="ridge", width=200, height=200)
 textbox = ttk.Entry(root, width=45) # entry widget is used to create a single-line text box
 textbox.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
@@ -75,28 +73,29 @@ button_theme= ttk.Button(root, text="Theme", width=1, command=change_theme)
 # Place buttons on the grid
 
 # Bottom Row
-button_1.grid(row=3, padx = 3, pady=3, column=0)
-button_2.grid(row=3, padx = 3, pady=3, column=1)
-button_3.grid(row=3, padx = 3, pady=3, column=2)
-button_add.grid(row=3, padx = 3, pady=3, column=3)
+button_1.grid(row=3, padx=3, pady=3, column=0)
+button_2.grid(row=3, padx=3, pady=3, column=1)
+button_3.grid(row=3, padx=3, pady=3, column=2)
+button_add.grid(row=3, padx=3, pady=3, column=3)
 
 # Middle Row
-button_4.grid(row=2, padx = 3, pady=3, column=0)
-button_5.grid(row=2, padx = 3, pady=3, column=1)
-button_6.grid(row=2, padx = 3, pady=3, column=2)
-button_subtract.grid(row=2, padx = 3, pady=3, column=3)
+button_4.grid(row=2, padx=3, pady=3, column=0)
+button_5.grid(row=2, padx=3, pady=3, column=1)
+button_6.grid(row=2, padx=3, pady=3, column=2)
+button_subtract.grid(row=2, padx=3, pady=3, column=3)
 
 # Top Row
-button_7.grid(row=1, padx = 3, pady=3, column=0)
-button_8.grid(row=1, padx = 3, pady=3, column=1)
-button_9.grid(row=1, padx = 3, pady=3, column=2)
-button_multiply.grid(row=1, padx = 3, pady=3, column=3)
+button_7.grid(row=1, padx=3, pady=3, column=0)
+button_8.grid(row=1, padx=3, pady=3, column=1)
+button_9.grid(row=1, padx=3, pady=3, column=2)
+button_multiply.grid(row=1, padx=3, pady=3, column=3)
 
-button_0.grid(row=4,padx = 3, pady=3, column=1)
-button_delete.grid(row=4, padx = 3, pady=3, column=0)
-button_period.grid(row=4, padx = 3, pady=3, column=2)
-button_total.grid(row=4, padx = 3, pady=3, column=3)
+button_0.grid(row=4,padx=3, pady=3, column=1)
+button_delete.grid(row=4, padx=3, pady=3, column=0)
+button_period.grid(row=4, padx=3, pady=3, column=2)
+button_total.grid(row=4, padx=3, pady=3, column=3)
 
 button_back.grid(row=5, ipadx=32, pady=3, column=0)
 button_theme.grid(row=0,column=5, ipadx=20)
+
 root.mainloop()
