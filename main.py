cfdc470de2f17a43ae98d3f264e239388efa3d03
TieFaul <tylerfaulhaber@gmail.com>
2024-07-14 17:41:35 -0500

    added sticky to buttons

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from tkinter import ttk
 # Main application window
 root = tk.Tk() # initializes the main window
 root.title("Tyler's Calculator")
-root.resizable(False, False)
+# root.resizable(False, False)
 
 # Create display widget
 textbox = ttk.Entry(root, width=45) # entry widget is used to create a single-line text box
-textbox.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+textbox.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
 
 # Theme
 root.tk.call("source", "azure.tcl")
@@ -76,30 +76,30 @@ button_theme= ttk.Button(root, text="Theme", width=1, command=change_theme)
 # Place buttons on the grid
 
 # Bottom Row
-button_1.grid(row=3, padx=3, pady=3, column=0)
-button_2.grid(row=3, padx=3, pady=3, column=1)
-button_3.grid(row=3, padx=3, pady=3, column=2)
-button_add.grid(row=3, padx=3, pady=3, column=3)
+button_1.grid(row=3, padx=3, pady=3, column=0, sticky="nsew")
+button_2.grid(row=3, padx=3, pady=3, column=1, sticky="nsew")
+button_3.grid(row=3, padx=3, pady=3, column=2, sticky="nsew")
+button_add.grid(row=3, padx=3, pady=3, column=3, sticky="nsew")
 
 # Middle Row
-button_4.grid(row=2, padx=3, pady=3, column=0)
-button_5.grid(row=2, padx=3, pady=3, column=1)
-button_6.grid(row=2, padx=3, pady=3, column=2)
-button_subtract.grid(row=2, padx=3, pady=3, column=3)
+button_4.grid(row=2, padx=3, pady=3, column=0, sticky="nsew")
+button_5.grid(row=2, padx=3, pady=3, column=1, sticky="nsew")
+button_6.grid(row=2, padx=3, pady=3, column=2, sticky="nsew")
+button_subtract.grid(row=2, padx=3, pady=3, column=3, sticky="nsew")
 
 # Top Row
-button_7.grid(row=1, padx=3, pady=3, column=0)
-button_8.grid(row=1, padx=3, pady=3, column=1)
-button_9.grid(row=1, padx=3, pady=3, column=2)
-button_multiply.grid(row=1, padx=3, pady=3, column=3)
+button_7.grid(row=1, padx=3, pady=3, column=0, sticky="nsew")
+button_8.grid(row=1, padx=3, pady=3, column=1, sticky="nsew")
+button_9.grid(row=1, padx=3, pady=3, column=2, sticky="nsew")
+button_multiply.grid(row=1, padx=3, pady=3, column=3, sticky="nsew")
 
-button_0.grid(row=4,padx=3, pady=3, column=1)
-button_delete.grid(row=4, padx=3, pady=3, column=0)
-button_period.grid(row=4, padx=3, pady=3, column=2)
-button_total.grid(row=4, padx=3, pady=3, column=3)
+button_0.grid(row=4,padx=3, pady=3, column=1, sticky="nsew")
+button_delete.grid(row=4, padx=3, pady=3, column=0, sticky="nsew")
+button_period.grid(row=4, padx=3, pady=3, column=2, sticky="nsew")
+button_total.grid(row=4, padx=3, pady=3, column=3, sticky="nsew")
 
-button_remove.grid(row=5, ipadx=32, pady=3, column=0)
-button_divide.grid(row=5, pady=3, column=3)
-button_theme.grid(row=0,column=5, ipadx=20)
+button_remove.grid(row=5, ipadx=32, pady=3, column=0, sticky="nsew")
+button_divide.grid(row=5, pady=3, column=3, sticky="nsew")
+button_theme.grid(row=0,column=5, ipadx=20, sticky="nsew")
 
 root.mainloop()
